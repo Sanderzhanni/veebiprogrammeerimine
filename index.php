834f58ff5d2192e7ae0f15f8543dae974cc11bba
Sander Hanni <Sanderzhanni@users.noreply.github.com>
2018-09-10 10:40:48 +0300

    html muudetud php'ks

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Sander Hanni, veebiprogrammeerimine</title>
+	</head>
+	<body>
+		<h1>Sander Hanni</h1>
+		<p>Siin on minu
+			<a href="http://www.tlu.ee" target=_blank">TLÜ</a>
+		õppetöö raames valminud veebilehed, need ei hõlma sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+		
+		<p>Minu kaasüliõpilase veebi leiate 
+		<a href="../../~gertpak" target=_blank">siit</a>
+		.</p>
+		<!--<img src=
+			"http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" 
+			alt=
+			"TLÜ Terra õppehoone">-->
+			<img src=
+				"../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" 
+				alt=
+				"TLÜ Terra õppehoone">
+		<p> </p>
+			<img src=
+				"https://img.memecdn.com/html_o_207686.webp" 
+				alt=
+				"meme.html">
+		
+	</body>
+</html>
